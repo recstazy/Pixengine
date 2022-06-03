6d336ceb2141c8ed4b72a644c577c77c9e213511
Stanislav Bukotkin <djexstas9@gmail.com>
2022-06-03 15:32:45 +0600

    Added creating Vulkan instance

--- Pixengine/MainApplication.cpp
+++ Pixengine/MainApplication.cpp
@@ -1,4 +1,7 @@
 #include "MainApplication.h"
+#include <stdexcept>
+#include <vector>
+#include <iostream>
 
 void MainApplication::run()
 {
@@ -20,6 +23,7 @@ void MainApplication::InitWindow()
 
 void MainApplication::InitVulkan()
 {
+    CreateVulkanInstance();
 }
 
 void MainApplication::MainLoop()
@@ -31,7 +35,50 @@ void MainApplication::MainLoop()
 
 void MainApplication::Cleanup()
 {
+    vkDestroyInstance(vkInstance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
     window = nullptr;
+}
+
+void MainApplication::CreateVulkanInstance()
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Pixengine";
+    appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    LogGlfwExtensions(glfwExtensionCount, glfwExtensions);
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &vkInstance);
+
+    if (result != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create instance!");
+    }
+}
+
+void MainApplication::LogGlfwExtensions(uint32_t extensionCount, const char** glfwExtensions)
+{
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    std::cout << "available extensions:\n";
+
+    for (const auto& extension : extensions) {
+        std::cout << '\t' << extension.extensionName << '\n';
+    }
 }
--- Pixengine/MainApplication.h
+++ Pixengine/MainApplication.h
@@ -7,7 +7,8 @@ class MainApplication
 private:
     const int WIDTH = 640;
     const int HEIGHT = 480;
-    GLFWwindow *window;
+    GLFWwindow* window;
+    VkInstance vkInstance;
 
 public:
     void run();
@@ -17,5 +18,7 @@ private:
     void InitVulkan();
     void MainLoop();
     void Cleanup();
+    void CreateVulkanInstance();
+    void LogGlfwExtensions(uint32_t extensionsCount, const char** glfwExtensions);
 };
 
